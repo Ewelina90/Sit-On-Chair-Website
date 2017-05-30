2bb80a921d077f9855ab96e0cc6d5ebce27ebca5
Ewelina Dziura <ewedziura@gmail.com>
2017-05-30 13:28:07 +0200

    slaider

--- js/app.js
+++ js/app.js
@@ -20,5 +20,34 @@ document.addEventListener("DOMContentLoaded",function(){
         });
     });
 
+    var currentImg = 0;
+    var leftNav = document.querySelector('.slaider-navigation-left');
+    var rightNav = document.querySelector('.slaider-navigation-right');
+    var slaiderImages = [...document.querySelectorAll('#slaider-images img')];
+
+    rightNav.addEventListener('click', function(){
+        slaiderImages[currentImg].classList.remove('slaider_image');
+        slaiderImages[currentImg].classList.add('invisible');
+        currentImg += 1;
+        if(!(currentImg < slaiderImages.length)){
+                currentImg = 0;
+        }
+        slaiderImages[currentImg].classList.remove('invisible');
+        slaiderImages[currentImg].classList.add('slaider_image')
+
+    });
+
+    leftNav.addEventListener('click', function(){
+        slaiderImages[currentImg].classList.remove('slaider_image');
+        slaiderImages[currentImg].classList.add('invisible');
+        currentImg -= 1;
+        if(currentImg < 0){
+                currentImg = slaiderImages.length - 1;
+        }
+        slaiderImages[currentImg].classList.remove('invisible');
+        slaiderImages[currentImg].classList.add('slaider_image')
+
+    });
+
 
 });
